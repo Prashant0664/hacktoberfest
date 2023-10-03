686e8fbf1627e6c004d76f4f036af9e24e0878df
Jayant Patidar <76242662+jpatidar9826@users.noreply.github.com>
2023-10-03 18:54:39 +0530

    Create 3Sum.cpp

---
+++ C++/3Sum.cpp
@@ -0,0 +1,45 @@
+// LeetCode Problem Link :- https://leetcode.com/problems/3sum/
+// Author :- Jayant Patidar
+
+/*
+
+    Time Complexity : O(N^2), Here Two nested loop creates the time complexity. Where N is the size of the
+    array(nums).
+
+    Space Complexity : O(1), Constant space. Extra space is only allocated for the Vector(output), however the
+    output does not count towards the space complexity.
+
+    Solved using Array(Two Nested Loop) + Sorting. Optimized Approach.
+
+*/
+
+
+/***************************************** Approach *****************************************/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        for(int i=0; i<n-1; i++){
+            int low = i+1, high = n-1;
+            while(low < high){
+                if(nums[i] + nums[low] + nums[high] < 0){
+                    low++;
+                }
+                else if(nums[i] + nums[low] + nums[high] > 0){
+                    high--;
+                }
+                else{
+                    output.push_back({nums[i], nums[low], nums[high]});
+                    int tempIndex1 = low, tempIndex2 = high;
+                    while(low < high && nums[low] == nums[tempIndex1]) low++;
+                    while(low < high && nums[high] == nums[tempIndex2]) high--;
+                }
+            }
+            while(i+1 < n && nums[i] == nums[i+1]) i++;
+        }
+        return output;
+    }
+};
